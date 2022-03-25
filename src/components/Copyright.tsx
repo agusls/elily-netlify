c283159bcba658defdf519f8d5ba450dd3658e70
agusls <wilgautama@gmail.com>
2022-03-26 02:46:14 +0700

    test

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export default function Copyright() {
+  return (
+    <>
+      <p>&copy; 2022 - agusls blog</p>
+      <style jsx>
+        {`
+          p {
+            font-size: 0.75rem;
+            text-align: center;
+          }
+        `}
+      </style>
+    </>
+  );
+}
